93226a134e3b2375532b0a8d3498f08898b3e175
unknown <jeromesloss@yahoo.com>
2023-09-07 13:59:10 -0400

    all music routes set up

--- backend/routes/musicRoutes.js
+++ backend/routes/musicRoutes.js
@@ -1,4 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Get music' })
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Set music' })
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `Update music ${req.params.id}` })
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: `Delete music ${req.params.id}` })
+})
+
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api/music', (req, res) => {
-    res.status(200).json({ message: 'Get music' })
-})
+app.use('/api/music', require('./routes/musicRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
