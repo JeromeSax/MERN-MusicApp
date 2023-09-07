5220546032071a8b5557be236e5f72f551a6dbe0
unknown <jeromesloss@yahoo.com>
2023-09-07 16:02:46 -0400

    new dependency installed, musicController filed edited

--- backend/controllers/musicController.js
+++ backend/controllers/musicController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get music
 // @route GET /api/music
 // @access Private
-const getMusic = (req, res) => {
+const getMusic = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get music' })
-}
+})
 
 // @desc Set music
 // @route POST /api/music
 // @access Private
-const setMusic = (req, res) => {
+const setMusic = asyncHandler(async (req, res) => {
     if(!req.body.text) {
       res.status(400)
       throw new Error('Please add a text field')
     }
 
     res.status(200).json({ message: 'Set music' })
-}
+})
 
 // @desc Update music
 // @route PUT /api/music/:id
 // @access Private
-const updateMusic = (req, res) => {
+const updateMusic = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update music ${req.params.id}` })
-}
+})
 
 // @desc Delete music
 // @route DELETE /api/music/:id
 // @access Private
-const deleteMusic = (req, res) => {
+const deleteMusic = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete music ${req.params.id}` })
-}
+})
 
 module.exports = {
     getMusic,
