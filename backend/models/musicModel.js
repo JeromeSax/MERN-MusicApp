5b60c155c934497e8320e30d9bac88e80ec21061
unknown <jeromesloss@yahoo.com>
2023-09-09 19:15:47 -0400

    musicModel edited, user added to schema

--- backend/models/musicModel.js
+++ backend/models/musicModel.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
 
-const musicSchema = mongoose.Schema({
-    text: {
-        type: String,
-        required: [true, 'Please add a text value']
+const musicSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        text: {
+            type: String,
+            required: [true, 'Please add a text value']
+        },
     },
-}, {
-    timestamps: true,
-})
+    {
+        timestamps: true,
+    }
+)
 
 module.exports = mongoose.model('Music', musicSchema)
